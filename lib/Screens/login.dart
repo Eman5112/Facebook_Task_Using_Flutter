6f5eabfcd124f0eb6d2d8e38b3a48c414fed185d
Eman5112 <115691759+Eman5112@users.noreply.github.com>
2025-08-17 21:53:40 +0300

    Update login.dart

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -6,6 +6,8 @@ import 'package:course_flutter/widgets/logo_widget.dart';
 import 'package:course_flutter/widgets/text_input_widget.dart';
 import 'package:course_flutter/widgets/login_button_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:course_flutter/Screens/register.dart'; // Import the Register widget
+import 'package:course_flutter/Screens/forget_password.dart'; // Import the ForgetPassword widget
 class Login extends StatelessWidget {
   const Login({super.key});
 
@@ -21,9 +23,14 @@ class Login extends StatelessWidget {
           TextInputWidget(TextFieldText: 'Email or phone number',keyboardType: TextInputType.emailAddress,prefixIcon: Icons.email,),
           TextInputWidget(TextFieldText: 'Password', isObsecure: true,sizedBoxHeight: 25,prefixIcon: Icons.password),
           LoginButtonWidget(btnText: 'Login'),
-          CustomTextButtonWidget(btnText: 'Forget Password?',),
+          CustomTextButtonWidget(btnText: 'Forget Password?'
+          ,onClick: (){
+            Navigator.push(context,MaterialPageRoute(builder:(context)=> ForgetPassword() ) );
+          },),
           CustomDividerWidget(),
-          CustomOutlinedButtonWidget(btnText: 'Create New Password',onClick: (){
+          CustomOutlinedButtonWidget(btnText: 'Create New Password',
+          onClick: (){
+            Navigator.push(context,MaterialPageRoute(builder:(context)=> Register() ) );
           },)
           
         ],
