c86a7e9244f81f46fc9a4be6031b3083fa2716dc
Eman5112 <115691759+Eman5112@users.noreply.github.com>
2025-08-17 21:56:51 +0300

    Update custom_text_button_widget.dart

--- lib/widgets/custom_text_button_widget.dart
+++ lib/widgets/custom_text_button_widget.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class CustomTextButtonWidget extends StatelessWidget{
   String btnText;
+  Function onClick;
+  VoidCallback? onPressed;
   CustomTextButtonWidget({
-    required this.btnText});
+    required this.btnText,
+    required this.onClick,});
   @override
   Widget build(BuildContext context) {
     return Column(
